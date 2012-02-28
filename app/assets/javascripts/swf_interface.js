3ea9196a0e0d0083e34bd0812320b1e369765b09
Jason Mckim <jasonbmc10@gmail.com>
2012-02-28 08:53:36 -0500

    window scope addedc to city id.

--- app/assets/javascripts/swf_interface.js
+++ app/assets/javascripts/swf_interface.js
@@ -11,7 +11,6 @@
  * needs to be followed by resumeSwf()
  */
 
-var cCityId ="city-1";
 var urlParams = {};
 		(function () {
 		    var e,
@@ -24,14 +23,7 @@ var urlParams = {};
 		      urlParams[d(e[1])] = d(e[2]);
 		 })();
 		 
-function submitMedia(city_code,mediatype){
-	var mcode = (mediatype=="image") ? "photo" : "video";
-	cCityId = city_code;
-	trackMosaicEvent('upload',mcode,'topnav');
-	pauseSwf();
-}
-
-
+		 
 //Tracking
  var _gaq = _gaq || [];
  _gaq.push(['_setAccount', 'UA-29233609-1']);
@@ -43,14 +35,94 @@ function submitMedia(city_code,mediatype){
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
   
-
-
 function trackMosaicEvent(eventId,mediaType,evtSource){
+	var cCityId = (window.city_id_code!=undefined && window.city_id_code!='') ? "city-"+window.city_id_code : "city-1";
 	var mt = (evtSource!="") ? evtSource+"-"+mediaType : mediaType;
 	_gaq.push(['_trackEvent', cCityId, eventId, mt]);
+}
+
+function submitMedia(city_code,mediatype){
+	trackMosaicEvent('upload',mcode,'topnav');
+	pauseSwf();
+
+	try{
+			var uploadtype = "";
+			if( mediatype=="video") {
+				uploadtype="?video=accept";
+				$('label[for="picture_remote_file_url"]').html('Link Video');
+				Cufon.replace( $('label[for="picture_remote_file_url"]') );
+			}
+			$('#swf_div')[0].pauseSwf();
+			var src = "http://"+request_HTTP_HOST+"/cities/"+city_code.split('-')[1]+"/pictures/new"+uploadtype;
+			$.modal('<iframe src="' + src + '" height="350" width="399" style="border:0">', {
+				closeHTML:"close",
+				containerCss:{
+					background:"url(/assets/upload-modal-global.png) no-repeat transparent",
+					width: 420,
+					height:360,
+					paddingTop: 15,
+					paddingBottom: 5 
+				},
+				position: [60,],
+				overlayClose:true,
+				onOpen: function(dialog){
+					dialog.overlay.fadeIn('slow', function () {
+						dialog.data.hide();
+						dialog.container.fadeIn('slow', function () {
+							dialog.data.slideDown('slow');
+						});
+					});
+
+				},
+				onClose: function(dialog) {
+					console.log('DIALOG');
+					console.log( dialog );
+					dialog.data.fadeOut('slow', function () {
+						dialog.container.hide('slow', function () {
+							dialog.overlay.slideUp('slow', function () {
+								$.modal.close();
+								if( window.mosaic_fb_share===true){
+								// calling the API ...
+								var obj = {
+								  method: 'feed',
+								  link: facebook_app_url + '/?controller=cities&id='+city_id+'&action=interative',
+								  picture: request_HTTP_HOST + "/" + city_mosaic,
+								  name: city_name + ' Intel Mosaic with Will.i.am',
+								  caption: 'Join the mosaic by submitting media that best represents you and your city.',
+								  description: ''
+								};
+								console.log(obj);
+
+								FB.ui(obj, fb_callback);
+								}else{
+  									resumeSwf();
+								}
+
+
+							});
+						});
+					});
+				}
+			});
+
+			// Closing animations
+			$("#sample").modal({});
+			
+
+		} catch (e){}
 	
 }
 
+
+function fb_callback(response) {
+  resumeSwf();
+}
+
+
+function joinTheProjectClick(){
+	top.window.location = "http://apps.facebook.com/368266769857206/?controller=cities&id="+city_id+"&action=interactive";
+	
+}
 
 /*
  * called on user select of language drop down. 
@@ -67,9 +139,9 @@ function setLanguage(loc_code){
  */
 function setCityMosaic(mosaic_code){
 	
-	
-	console.log("setCityMosaic"+mosaic_code);
-	//top.window.location = "https://apps.facebook.com/testing_url/"+"?city_id="+mosaic_code;
+
+
+	top.window.location = "http://apps.facebook.com/368266769857206/?controller=cities&id="+mosaic_code+"&action=interactive";
 }
 /*
  * Can bee used to implement tracking; will get called when app state changes 
@@ -78,22 +150,23 @@ function setCityMosaic(mosaic_code){
 function mosaicStateUpdate(cityid,appstate,behaviortrigger,itemid){
 	
 }
+
+/*
+ * calls Swf pause function...(like when displaying a modal above)
+ */
+function pauseSwf(d){
+	
+}
+/*
+ * different user logs in; or on log out: pass an ivalid id like -1
+ * swf will modify findme behaviors for this mosaic.
+ */
 function setUserId(s){
 	try{
 		$('#swf_div')[0].setUserId(s);
 	} catch (e){}
 }
-/*
- * calls Swf pause function...(like when displaying a modal above)
- */
-function pauseSwf(){
-	try{
-		$('#swf_div')[0].pauseSwf();
-	} catch (e){}
-}
-function joinTheProjectClick(){
-	setCityMosaic(1);
-}
+
 /*
  * calls Swf to resume from paused state.
  */
@@ -103,8 +176,11 @@ function resumeSwf(){
 	} catch (e){}
 }
 
+
+
 function jslog(s){
 	try{
-		//console.log(s);
+		console.log('LOG');
+		console.log(s);
 	} catch (e){};
 }
