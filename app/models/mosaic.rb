ce1771cd6e72c63e56a9f19a87512c1ae4118bb9
mikereed <mreed@kbsp.com>
2012-03-02 01:37:04 -0500

    Generate mosaics on prod using sleep in shell scripts.  Probably should run in background

--- app/models/mosaic.rb
+++ app/models/mosaic.rb
@@ -16,5 +16,25 @@ class Mosaic < ActiveRecord::Base
     logger.debug( @mosaic.inspect )
     "/mosaics/city-"+a_cityid.to_s+"/"+@mosaic.id.to_s+".jpg"
   end
+  
+  # GET /cities/:city_id/mosaics/generate
+  # GET /cities/:city_id/mosaics/generate.json
+  def self.generate_city(city_to_generate = 'Seoul')
+    logger.info('GENERATE CITY IN MOSAIC MODEL FIRED')
+    #@city = City.find_by_name(params['city_id'])
+    if city_to_generate.nil?
+      @city = City.find_last_by_status('open')
+    else
+      @city = City.find_by_name(city_to_generate)
+    end
+    
+    Resque.enqueue( MosaicGenerator, @city.id )
+    logger.debug( @city.inspect )
+    #respond_to do |format|
+      #format.html #generate.html
+      #format.json { render json: ( @city ) }
+    #end
+  end
+
 end
 
